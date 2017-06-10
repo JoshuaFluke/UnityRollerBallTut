9fb9aa18a6d2052f09de880ec5a83b4d05129d55
Joshua Fluke <jfluke717@gmail.com>
2017-06-09 19:26:48 -0600

    script to move the ball

---
+++ PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+
+    private void FixedUpdate()
+    {
+        float strafe = Input.GetAxis("Horizontal");
+        float forwardBackward = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(strafe, 0.0f, forwardBackward);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
